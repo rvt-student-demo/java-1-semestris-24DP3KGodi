77303afad6794235b16f647a6bf30c94444da9a1
24DP3KGodi <A242608KG@rvt.lv>
2025-12-04 18:54:23 +0000

    Chapter60

---
+++ src/main/java/rvt/Chapter60.java
@@ -0,0 +1,18 @@
+package rvt;
+import java.util.Scanner;
+
+public class Chapter60 {
+    
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int[] skaitlli = {6,8, 10, 2, 6};    
+
+    for (int i = 0 ; i < skaitlli.length; i++) {
+        System.out.println(skaitlli[i]);
+    }
+
+    scanner.close();
+    }
+}
+
